294edf5fa3d97ed03551462f1fe5e859294efd41
kochjar <timgfx@outlook.com>
2023-03-09 19:12:55 +0100

    Add project files.

---
+++ Ex14-Sorting/IntArrayHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex14_Sorting
+{
+    public class IntArrayHelper
+    {
+        public static int[] Sort(int[] array)
+        {
+            int temp = 0;  
+            for (int i=0; i <= array.Length-1; i++)
+            {
+                for(int j=i+1; j<array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+
+            return array;
+        }
+
+        public static int[] Reverse(int[] array)
+        {
+            int temp = 0;
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] < array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+
+            return array;
+        }
+
+        public static void Print(int[] array)
+        {
+            Console.Write("Array: ");
+            foreach (var item in array)
+            {
+                Console.Write(item+" ");
+            }
+            Console.Write("\n");
+        }
+    }
+}
---
+++ Ex14-Sorting/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ex14_Sorting
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[] { 1, 4, 2, 5, 3, 8, 1};
+
+
+            IntArrayHelper.Print(array);
+
+            IntArrayHelper.Sort(array);
+            
+            IntArrayHelper.Print(array);
+
+            IntArrayHelper.Reverse(array);
+
+            IntArrayHelper.Print(array);
+
+
+        }
+    }
+}
